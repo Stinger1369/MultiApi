290a2143db49a1ea3d6fa2e4514bad502d7f757e
bilel zaaraoui <bilelzara@gmail.com>
2023-11-29 17:06:50 +0100

    2023-11-29-17h06-BilelContact
    Contact

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,13 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Contact.css';
 
 function Contact() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission logic here
+    console.log(formData);
+    alert('Message sent!');
+  };
+
   return (
-    <div>
-      <h1>Contactez-Nous</h1>
-      <p>
-        Si vous avez des questions ou des suggestions concernant notre application, n'hésitez pas à nous contacter.
-      </p>
-      {/* Vous pouvez ajouter un formulaire de contact ou d'autres informations ici */}
+    <div className="contact-container">
+      <h1>Contact Us</h1>
+      <form onSubmit={handleSubmit} className="contact-form">
+        <input 
+          type="text" 
+          name="name" 
+          placeholder="Your Name" 
+          value={formData.name} 
+          onChange={handleChange} 
+        />
+        <input 
+          type="email" 
+          name="email" 
+          placeholder="Your Email" 
+          value={formData.email} 
+          onChange={handleChange} 
+        />
+        <textarea 
+          name="message" 
+          placeholder="Your Message" 
+          value={formData.message} 
+          onChange={handleChange} 
+        />
+        <button type="submit">Send</button>
+      </form>
     </div>
   );
 }
